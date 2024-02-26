21397fd8a54b7c6242c95582561d254b06f1eb53
Ira Akulova <99876200+IraAkulova@users.noreply.github.com>
2024-02-26 14:46:49 +0200

    Delete dist directory

--- dist/src/api/auth/auth.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AuthDto {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    role?: 'user' | 'boss' | 'admin';
-    bossId?: string;
-    adminId?: string;
-}
